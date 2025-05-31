68b79b22add53deffcabb43489e10ced0667bff7
ASH <77592824+Ahmed-GaMz@users.noreply.github.com>
2025-05-31 03:57:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -112,5 +112,5 @@ def uploaded_file(filename):
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
+        db.create_all()  # هذا اللي يبني قاعدة البيانات والجداول
     app.run(debug=True)
